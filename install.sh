51ff2405368d6b7fc1df13baaff70fd50ec86127
Nigel Stanger <nigel.stanger@otago.ac.nz>
2016-11-30 10:58:54 +1300

    • Added mkdir -p in case the target directory doesn’t exist.

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ set dir=$1
 foreach infile ($dir/*.applescript)
     set outfile=`basename $infile | sed -e 's/applescript$/scpt/'`
     osacompile -o "$outfile" "$infile"
+    mkdir -p "$HOME/Library/Scripts/Applications/$dir"
     mv "$outfile" "$HOME/Library/Scripts/Applications/$dir"
 end
