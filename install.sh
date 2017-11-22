49e303b9059256be2292453ea68c81e44ff80518
Nigel Stanger <nigel.stanger@otago.ac.nz>
2017-11-22 15:06:58 +1300

    • Adjusted for change in name of rename binary (irony!).

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /opt/local/bin/gfind . -name "*.applescript" -execdir osacompile -o "{}.scpt" "{}" \;
-/opt/local/bin/gfind . -name "*.scpt" -execdir rename-5.24 -force 's/\.applescript\.scpt/.scpt/' "{}" \;
+/opt/local/bin/gfind . -name "*.scpt" -execdir rename -force 's/\.applescript\.scpt/.scpt/' "{}" \;
 /opt/local/bin/rsync -av --exclude="*.applescript" Applications $HOME/Library/Scripts
 /opt/local/bin/rsync -av --exclude="*.applescript" "Script Libraries" $HOME/Library
 
