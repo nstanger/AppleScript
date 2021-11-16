d0af6d6ac2e8618c92dd1ebd32c0346e93178e8e
Nigel Stanger <nigel.stanger@otago.ac.nz>
2021-11-16 14:24:02 +1300

    Changed clean to git-clean

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-/opt/local/bin/gfind . -name "*.applescript" -execdir osacompile -o "{}.scpt" "{}" \;
-/opt/local/bin/gfind . -name "*.scpt" -execdir rename -force 's/\.applescript\.scpt/.scpt/' "{}" \;
-/opt/local/bin/rsync -av --exclude="*.applescript" Applications $HOME/Library/Scripts
-/opt/local/bin/rsync -av --exclude="*.applescript" "Script Libraries" $HOME/Library
-
 if [ "$1" = "--clean" ]
 then
-    /opt/local/bin/gfind -name "*.scpt" -delete
+    /opt/local/bin/git clean -fXd
+    exit 0
 fi
