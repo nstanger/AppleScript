feb7459a7d7e69aaac9b749d5918d1f3f12d7ea5
Nigel Stanger <nigel.stanger@otago.ac.nz>
2021-11-16 14:25:01 +1300

    Ignored .git folder in find

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ then
     /opt/local/bin/git clean -fXd
     exit 0
 fi
+
+/opt/local/bin/gfind . -path ./.git -prune -o -name "*.applescript" -execdir osacompile -o "{}.scpt" "{}" \;
+/opt/local/bin/gfind . -path ./.git -prune -o -name "*.scpt" -execdir rename -force 's/\.applescript\.scpt/.scpt/' "{}" \;
+/opt/local/bin/rsync -av --exclude="*.applescript" Applications $HOME/Library/Scripts
+/opt/local/bin/rsync -av --exclude="*.applescript" "Script Libraries" $HOME/Library
